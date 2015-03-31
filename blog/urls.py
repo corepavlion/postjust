142c110e1dc2a76744b58b18645f435cc8a2e1e6
Erkan Demir <erkandemir86@gmail.com>
2015-04-01 00:10:12 +0300

    blogpost list and blogpost detail added

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import patterns, include, url
 from blog import views
 
 urlpatterns = patterns('',
-	url(r'^blog/', views.blogPostList, name ='blogPostList')
+	url(r'^$', views.blogPostList, name ='blogPostList'),
+	url(r'^(?P<slug>\S+)/', views.blogDetailPage, name ='blogDetailPage')
 )
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from blog.models import BlogPost
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
-
-	
-
 def blogPostList(request):
-	return HttpResponse('blogs')
-	#return render(request, 'blogMainPage.html', {'currentPage' : currentPage})
+	postList = BlogPost.objects.all().order_by('-id')[:5]
+	return render(request, 'blogMainPage.html', {'postList' : postList})
+
+def blogDetailPage(request, slug):
+	blogPost = get_object_or_404(BlogPost, slug = slug)
+	return render(request, 'blogDetailPage.html', {'post' : blogPost})
