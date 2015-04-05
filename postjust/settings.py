c088d474ca3e7eedb1b13860808f9196537e8711
Erkan Demir <erkandemir86@gmail.com>
2015-04-05 20:22:21 +0300

    update req.txt

--- postjust/settings.py
+++ postjust/settings.py
@@ -90,5 +90,5 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
-STATIC_ROOT = '/assests/'
-STATIC_URL = '/assets/'
+STATIC_ROOT = '/static/'
+STATIC_URL = '/static/'
