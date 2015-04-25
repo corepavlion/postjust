366529456693832bc0bffee13a6eb6109c48eab2
Erkan Demir <erkandemir86@gmail.com>
2015-04-26 02:22:19 +0300

    update

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .models import BlogPost, BlogCategory
 
-# Create your tests here.
+class BlogPostMethodTests(TestCase):
+
+    def test_category_count(self):
+        category = BlogCategory()
+        category.title = 'test'
+        category.save()
+
+        blogPost = BlogPost()
+        blogPost.title = 'test'
+        blogPost.save()
+
+        blogPost.categories.add(category)
+
+        self.assertEqual(blogPost.categories.all().count(), 1)
