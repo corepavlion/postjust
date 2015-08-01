63fd4ab62b191d788269e0cea08bbd1563a89990
Erkan Demir <erkandemir86@gmail.com>
2015-08-02 02:41:04 +0300

    db change to sqlite3 again on prod

--- postjust/settings_prod.py
+++ postjust/settings_prod.py
@@ -108,11 +108,19 @@ ALLOWED_HOSTS = ['*']
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {}
+# }
+# import dj_database_url
+# DATABASES['default'] =  dj_database_url.config()
+
 DATABASES = {
-    'default': {}
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
+
+    }
 }
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
 
 import sys
 if 'test' in sys.argv:
